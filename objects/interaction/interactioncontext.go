478edee2b18014daef5424af72bf86248042e2f5
biast12 <tobiaspoulsen@hotmail.com>
2025-09-27 15:54:17 +0200

    Refactor InteractionContextType to int8 and remove JSON methods
    Changed InteractionContextType from uint8 to int8 and removed custom MarshalJSON and UnmarshalJSON methods, simplifying the type and its usage.

--- objects/interaction/interactioncontext.go
+++ objects/interaction/interactioncontext.go
@@ -1,24 +1,9 @@
 package interaction
 
-import "encoding/json"
-
-type InteractionContextType uint8
+type InteractionContextType int8
 
 const (
 	InteractionContextGuild InteractionContextType = 0
 	InteractionContextBotDM InteractionContextType = 1
 	InteractionContextPrivateChannel InteractionContextType = 2
 )
-
-func (i InteractionContextType) MarshalJSON() ([]byte, error) {
-	return json.Marshal(uint8(i))
-}
-
-func (i *InteractionContextType) UnmarshalJSON(data []byte) error {
-	var val uint8
-	if err := json.Unmarshal(data, &val); err != nil {
-		return err
-	}
-	*i = InteractionContextType(val)
-	return nil
-}
