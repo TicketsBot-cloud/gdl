339100dd0ea099722025fbf2824c56e00ba3a142
biast12 <tobiaspoulsen@hotmail.com>
2026-03-02 22:19:31 +0100

    Add omitempty to Sticker JSON tags
    Mark optional Sticker struct fields with `omitempty` in their JSON tags (PackId, Description, Available, GuildId, User, SortValue) so empty or nil values are omitted when marshaling. This prevents null/zero fields from being serialized for optional sticker data. https://docs.discord.com/developers/resources/sticker#sticker-resource

--- objects/guild/sticker/sticker.go
+++ objects/guild/sticker/sticker.go
@@ -20,14 +20,14 @@ const (
 
 type Sticker struct {
 	Id          uint64            `json:"id,string"`
-	PackId      *uint64           `json:"pack_id,string"`
+	PackId      *uint64           `json:"pack_id,string,omitempty"`
 	Name        string            `json:"name"`
-	Description *string           `json:"description"`
+	Description *string           `json:"description,omitempty"`
 	Tags        string            `json:"tags"`
 	Type        StickerType       `json:"type"`
 	FormatType  StickerFormatType `json:"format_type"`
-	Available   *bool             `json:"available"`
-	GuildId     *uint64           `json:"guild_id,string"`
-	User        *user.User        `json:"user"`
-	SortValue   *int              `json:"sort_value"`
+	Available   *bool             `json:"available,omitempty"`
+	GuildId     *uint64           `json:"guild_id,string,omitempty"`
+	User        *user.User        `json:"user,omitempty"`
+	SortValue   *int              `json:"sort_value,omitempty"`
 }
