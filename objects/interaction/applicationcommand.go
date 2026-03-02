09a28d37bd04844df3fd577b49cc33cb8860143a
biast12 <tobiaspoulsen@hotmail.com>
2026-03-03 00:16:57 +0100

    Update application command structs and option types
    Extend and modernize ApplicationCommand and ApplicationCommandOption definitions: rename/standardize JSON tags, add new fields (guild_id, default_member_permissions, nsfw, version), and make several fields optional (options, focused, required, autocomplete). Add numeric/string bounds (min_value, max_value, min_length, max_length) for options and introduce a new ApplicationCommandType constant (PrimaryEntryPoint). Also rename option enum constants to more explicit ApplicationCommandOptionType* names. These changes align the types with updated API field names and optionality, and improve validation support for option values. https://docs.discord.com/developers/interactions/application-commands#application-commands

--- objects/interaction/applicationcommand.go
+++ objects/interaction/applicationcommand.go
@@ -1,14 +1,17 @@
 package interaction
 
 type ApplicationCommand struct {
-	Id                uint64                      `json:"id,string,omitempty"`
-	Type              ApplicationCommandType      `json:"application_command_type"`
-	ApplicationId     uint64                      `json:"application_id,string,omitempty"`
-	Name              string                      `json:"name"`
-	Description       string                      `json:"description"`
-	Options           []ApplicationCommandOption  `json:"options"`
-	DefaultPermission bool                        `json:"default_permission,omitempty"`
-	Contexts          []InteractionContextType    `json:"contexts,omitempty"`
+	Id                     uint64                     `json:"id,string,omitempty"`
+	Type                   ApplicationCommandType     `json:"type"`
+	ApplicationId          uint64                     `json:"application_id,string,omitempty"`
+	GuildId                *uint64                    `json:"guild_id,string,omitempty"`
+	Name                   string                     `json:"name"`
+	Description            string                     `json:"description"`
+	Options                []ApplicationCommandOption `json:"options,omitempty"`
+	DefaultMemberPermissions *string                  `json:"default_member_permissions,omitempty"`
+	Nsfw                   bool                       `json:"nsfw,omitempty"`
+	Version                uint64                     `json:"version,string,omitempty"`
+	Contexts               []InteractionContextType   `json:"contexts,omitempty"`
 }
 
 type ApplicationCommandType uint8
@@ -17,4 +20,5 @@ const (
 	ApplicationCommandTypeChatInput ApplicationCommandType = iota + 1
 	ApplicationCommandTypeUser
 	ApplicationCommandTypeMessage
+	ApplicationCommandTypePrimaryEntryPoint
 )
--- objects/interaction/applicationcommandoption.go
+++ objects/interaction/applicationcommandoption.go
@@ -6,35 +6,38 @@ type ApplicationCommandInteractionDataOption struct {
 	Name    string                                    `json:"name"`
 	Value   interface{}                               `json:"value,omitempty"`
 	Options []ApplicationCommandInteractionDataOption `json:"options,omitempty"`
-	Focused bool                                      `json:"focused"`
+	Focused bool                                      `json:"focused,omitempty"`
 }
 
 type ApplicationCommandOption struct {
 	Type         ApplicationCommandOptionType     `json:"type"`
 	Name         string                           `json:"name"`
 	Description  string                           `json:"description"`
-	Default      bool                             `json:"default"`
-	Required     bool                             `json:"required"`
+	Required     bool                             `json:"required,omitempty"`
 	Choices      []ApplicationCommandOptionChoice `json:"choices,omitempty"`
-	Autocomplete bool                             `json:"autocomplete"`
+	Autocomplete bool                             `json:"autocomplete,omitempty"`
 	Options      []ApplicationCommandOption       `json:"options,omitempty"`
 	ChannelTypes []channel.ChannelType            `json:"channel_types,omitempty"`
+	MinValue     *float64                         `json:"min_value,omitempty"`
+	MaxValue     *float64                         `json:"max_value,omitempty"`
+	MinLength    *int                             `json:"min_length,omitempty"`
+	MaxLength    *int                             `json:"max_length,omitempty"`
 }
 
 type ApplicationCommandOptionType uint8
 
 const (
-	OptionTypeSubCommand ApplicationCommandOptionType = iota + 1
-	OptionTypeSubCommandGroup
-	OptionTypeString
-	OptionTypeInteger
-	OptionTypeBoolean
-	OptionTypeUser
-	OptionTypeChannel
-	OptionTypeRole
-	OptionTypeMentionable
-	OptionTypeNumber
-	OptionTypeAttachment
+	ApplicationCommandOptionTypeSubCommand ApplicationCommandOptionType = iota + 1
+	ApplicationCommandOptionTypeSubCommandGroup
+	ApplicationCommandOptionTypeString
+	ApplicationCommandOptionTypeInteger
+	ApplicationCommandOptionTypeBoolean
+	ApplicationCommandOptionTypeUser
+	ApplicationCommandOptionTypeChannel
+	ApplicationCommandOptionTypeRole
+	ApplicationCommandOptionTypeMentionable
+	ApplicationCommandOptionTypeNumber
+	ApplicationCommandOptionTypeAttachment
 )
 
 type ApplicationCommandOptionChoice struct {
