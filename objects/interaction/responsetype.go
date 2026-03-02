190b2e443f3a927aaff089379b916387b9e1a351
biast12 <tobiaspoulsen@hotmail.com>
2026-03-03 00:30:57 +0100

    Rename and realign interaction response types
    Rename several ResponseType constants and realign their numeric values. ACKWithSource was renamed to DeferredChannelMessageWithSource and DeferredMessageUpdate to DeferredUpdateMessage; response constructors were updated accordingly. Const blocks were restructured to adjust iota offsets (e.g. Pong is now 0), PremiumRequired marked deprecated, and a new LaunchActivity constant was added. Note: this changes constant names and their numeric assignments, which may be a breaking change for code relying on the previous values. https://docs.discord.com/developers/interactions/receiving-and-responding#interaction-response-object-interaction-callback-type

--- objects/interaction/responsetype.go
+++ objects/interaction/responsetype.go
@@ -3,14 +3,13 @@ package interaction
 type ResponseType uint8
 
 const (
-	ResponseTypePong ResponseType = iota + 1
-	_
-	_
-	ResponseTypeChannelMessageWithSource
-	ResponseTypeACKWithSource
-	ResponseTypeDeferredMessageUpdate
-	ResponseTypeUpdateMessage
-	ResponseTypeApplicationCommandAutoCompleteResult
-	ResponseTypeModal
-	ResponseTypePremiumRequired
+	ResponseTypePong                                 ResponseType = 1
+	ResponseTypeChannelMessageWithSource             ResponseType = 4
+	ResponseTypeDeferredChannelMessageWithSource     ResponseType = 5
+	ResponseTypeDeferredUpdateMessage                ResponseType = 6
+	ResponseTypeUpdateMessage                        ResponseType = 7
+	ResponseTypeApplicationCommandAutoCompleteResult ResponseType = 8
+	ResponseTypeModal                                ResponseType = 9
+	ResponseTypePremiumRequired                      ResponseType = 10 // Deprecated
+	ResponseTypeLaunchActivity                       ResponseType = 12
 )
