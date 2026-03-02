70d7f0af7b1dcf6181c9d0bb55112a4d09ccaf3e
biast12 <tobiaspoulsen@hotmail.com>
2026-03-03 00:42:24 +0100

    Use StagePrivacyLevel and nullable event ID
    Introduce StagePrivacyLevel enum and switch StageInstance.PrivacyLevel from int to this new type for stronger typing. Change GuildScheduledEventId from *uint64 to objects.NullableSnowflake to handle JSON nulls more explicitly and remove the `,string` tag. Make DiscoverableDisabled omit empty values in JSON and add the required import for objects. https://docs.discord.com/developers/resources/stage-instance#stage-instance-resource

--- objects/stage/stage.go
+++ objects/stage/stage.go
@@ -1,11 +1,20 @@
 package stage
 
+import "github.com/TicketsBot-cloud/gdl/objects"
+
+type StagePrivacyLevel int
+
+const (
+	StagePrivacyLevelPublic    StagePrivacyLevel = 1 // Deprecated
+	StagePrivacyLevelGuildOnly StagePrivacyLevel = 2
+)
+
 type StageInstance struct {
-	Id                    uint64  `json:"id,string"`
-	GuildId               uint64  `json:"guild_id,string"`
-	ChannelId             uint64  `json:"channel_id,string"`
-	Topic                 string  `json:"topic"`
-	PrivacyLevel          int     `json:"privacy_level"`
-	DiscoverableDisabled  bool    `json:"discoverable_disabled"`
-	GuildScheduledEventId *uint64 `json:"guild_scheduled_event_id,string"`
+	Id                    uint64                    `json:"id,string"`
+	GuildId               uint64                    `json:"guild_id,string"`
+	ChannelId             uint64                    `json:"channel_id,string"`
+	Topic                 string                    `json:"topic"`
+	PrivacyLevel          StagePrivacyLevel         `json:"privacy_level"`
+	DiscoverableDisabled  bool                      `json:"discoverable_disabled,omitempty"`
+	GuildScheduledEventId objects.NullableSnowflake `json:"guild_scheduled_event_id"`
 }
