cc2f0cb8e8d9058515047d86c46d97c5828f0e6e
biast12 <tobiaspoulsen@hotmail.com>
2026-03-02 21:54:20 +0100

    Refactor automoderation types & metadata
    Add KeywordPresetType and corresponding constants, and change TriggerMetadata.Presets to []KeywordPresetType for clearer preset semantics. Make ActionMetadata.ChannelId and DurationSeconds pointer fields with `omitempty` to represent optional JSON values. Replace Rule.ExemptRoles and ExemptChannels with utils.Uint64StringSlice to standardize uint64-from-string handling. Add import for utils and apply minor formatting/type cleanups. https://docs.discord.com/developers/resources/auto-moderation

--- objects/automoderation/automoderation.go
+++ objects/automoderation/automoderation.go
@@ -1,5 +1,7 @@
 package automoderation
 
+import "github.com/TicketsBot-cloud/gdl/utils"
+
 type EventType int
 
 const (
@@ -17,27 +19,35 @@ const (
 	TriggerTypeMemberProfile TriggerType = 6
 )
 
+type KeywordPresetType int
+
+const (
+	KeywordPresetTypeProfanity     KeywordPresetType = 1
+	KeywordPresetTypeSexualContent KeywordPresetType = 2
+	KeywordPresetTypeSlurs         KeywordPresetType = 3
+)
+
 type ActionType int
 
 const (
-	ActionTypeBlockMessage            ActionType = 1
-	ActionTypeSendAlertMessage        ActionType = 2
-	ActionTypeTimeout                 ActionType = 3
-	ActionTypeBlockMemberInteraction  ActionType = 4
+	ActionTypeBlockMessage           ActionType = 1
+	ActionTypeSendAlertMessage       ActionType = 2
+	ActionTypeTimeout                ActionType = 3
+	ActionTypeBlockMemberInteraction ActionType = 4
 )
 
 type TriggerMetadata struct {
-	KeywordFilter                []string `json:"keyword_filter"`
-	RegexPatterns                []string `json:"regex_patterns"`
-	Presets                      []int    `json:"presets"`
-	AllowList                    []string `json:"allow_list"`
-	MentionTotalLimit            int      `json:"mention_total_limit"`
-	MentionRaidProtectionEnabled bool     `json:"mention_raid_protection_enabled"`
+	KeywordFilter                []string            `json:"keyword_filter"`
+	RegexPatterns                []string            `json:"regex_patterns"`
+	Presets                      []KeywordPresetType `json:"presets"`
+	AllowList                    []string            `json:"allow_list"`
+	MentionTotalLimit            int                 `json:"mention_total_limit"`
+	MentionRaidProtectionEnabled bool                `json:"mention_raid_protection_enabled"`
 }
 
 type ActionMetadata struct {
-	ChannelId       uint64  `json:"channel_id,string"`
-	DurationSeconds int     `json:"duration_seconds"`
+	ChannelId       *uint64 `json:"channel_id,string,omitempty"`
+	DurationSeconds *int    `json:"duration_seconds,omitempty"`
 	CustomMessage   *string `json:"custom_message,omitempty"`
 }
 
@@ -47,15 +57,15 @@ type Action struct {
 }
 
 type Rule struct {
-	Id              uint64          `json:"id,string"`
-	GuildId         uint64          `json:"guild_id,string"`
-	Name            string          `json:"name"`
-	CreatorId       uint64          `json:"creator_id,string"`
-	EventType       EventType       `json:"event_type"`
-	TriggerType     TriggerType     `json:"trigger_type"`
-	TriggerMetadata TriggerMetadata `json:"trigger_metadata"`
-	Actions         []Action        `json:"actions"`
-	Enabled         bool            `json:"enabled"`
-	ExemptRoles     []uint64        `json:"exempt_roles"`
-	ExemptChannels  []uint64        `json:"exempt_channels"`
+	Id              uint64                  `json:"id,string"`
+	GuildId         uint64                  `json:"guild_id,string"`
+	Name            string                  `json:"name"`
+	CreatorId       uint64                  `json:"creator_id,string"`
+	EventType       EventType               `json:"event_type"`
+	TriggerType     TriggerType             `json:"trigger_type"`
+	TriggerMetadata TriggerMetadata         `json:"trigger_metadata"`
+	Actions         []Action                `json:"actions"`
+	Enabled         bool                    `json:"enabled"`
+	ExemptRoles     utils.Uint64StringSlice `json:"exempt_roles"`
+	ExemptChannels  utils.Uint64StringSlice `json:"exempt_channels"`
 }
