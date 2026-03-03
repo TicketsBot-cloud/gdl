6bb55455621afe7082bc377ea4511bb9c237a1c8
biast12 <tobiaspoulsen@hotmail.com>
2026-03-03 18:41:54 +0100

    Add localization, handler & integration types
    Update application command types to match new API fields: add name_localizations and description_localizations to ApplicationCommand, ApplicationCommandOption and option choices; add IntegrationTypes (using application.ApplicationIntegrationType) and a Handler field with a new ApplicationCommandHandlerType enum; add Type to ApplicationCommandInteractionDataOption. Also import the application package and adjust struct fields/order to accommodate these additions for improved localization and handler/integration support. https://docs.discord.com/developers/interactions/application-commands

--- objects/interaction/applicationcommandoption.go
+++ objects/interaction/applicationcommandoption.go
@@ -3,6 +3,7 @@ package interaction
 import "github.com/TicketsBot-cloud/gdl/objects/channel"
 
 type ApplicationCommandInteractionDataOption struct {
+	Type    ApplicationCommandOptionType              `json:"type"`
 	Name    string                                    `json:"name"`
 	Value   interface{}                               `json:"value,omitempty"`
 	Options []ApplicationCommandInteractionDataOption `json:"options,omitempty"`
@@ -10,18 +11,20 @@ type ApplicationCommandInteractionDataOption struct {
 }
 
 type ApplicationCommandOption struct {
-	Type         ApplicationCommandOptionType     `json:"type"`
-	Name         string                           `json:"name"`
-	Description  string                           `json:"description"`
-	Required     bool                             `json:"required,omitempty"`
-	Choices      []ApplicationCommandOptionChoice `json:"choices,omitempty"`
-	Autocomplete bool                             `json:"autocomplete,omitempty"`
-	Options      []ApplicationCommandOption       `json:"options,omitempty"`
-	ChannelTypes []channel.ChannelType            `json:"channel_types,omitempty"`
-	MinValue     *float64                         `json:"min_value,omitempty"`
-	MaxValue     *float64                         `json:"max_value,omitempty"`
-	MinLength    *int                             `json:"min_length,omitempty"`
-	MaxLength    *int                             `json:"max_length,omitempty"`
+	Type                     ApplicationCommandOptionType     `json:"type"`
+	Name                     string                           `json:"name"`
+	NameLocalizations        map[string]string                `json:"name_localizations,omitempty"`
+	Description              string                           `json:"description"`
+	DescriptionLocalizations map[string]string                `json:"description_localizations,omitempty"`
+	Required                 bool                             `json:"required,omitempty"`
+	Choices                  []ApplicationCommandOptionChoice `json:"choices,omitempty"`
+	Autocomplete             bool                             `json:"autocomplete,omitempty"`
+	Options                  []ApplicationCommandOption       `json:"options,omitempty"`
+	ChannelTypes             []channel.ChannelType            `json:"channel_types,omitempty"`
+	MinValue                 *float64                         `json:"min_value,omitempty"`
+	MaxValue                 *float64                         `json:"max_value,omitempty"`
+	MinLength                *int                             `json:"min_length,omitempty"`
+	MaxLength                *int                             `json:"max_length,omitempty"`
 }
 
 type ApplicationCommandOptionType uint8
@@ -41,6 +44,7 @@ const (
 )
 
 type ApplicationCommandOptionChoice struct {
-	Name  string      `json:"name"`
-	Value interface{} `json:"value"` // string, int or double
+	Name              string            `json:"name"`
+	NameLocalizations map[string]string `json:"name_localizations,omitempty"`
+	Value             interface{}       `json:"value"` // string, int or double
 }
